73db6effffd6fa2142a359ed57808658cb898157
Siavash <kosarisiavash4@gmail.com>
2022-12-22 17:31:32 +0330

    combined ir and mp3 codes

--- harp/sketch_dec01a/sketch_dec01a.ino
+++ harp/sketch_dec01a/sketch_dec01a.ino
@@ -1,25 +1,56 @@
-#define IRPin 2
-#define model 1080
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 
-int distance;
-int a = 0;
+int i = 0;
+#define IRSensor 9
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+#define C4 0
+#define D4 1
+#define E4 2
+#define F4 3
+#define G4 4
+#define A4 5
+#define B4 6
 
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+void setup()
+{
+
+  pinMode(IRSensor, INPUT); // set pinmod for ir sensor to output
+  
+  mySoftwareSerial.begin(9600);
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    while(true);
+  }
+
+  
+  myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
+  
+  //----Set volume----
+  myDFPlayer.volume(25);  //Set volume value (0~30).
+  
+  //----Set different EQ----
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  
+  //----Set device we use SD as default----
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
 }
 
-void loop() {
-  for (int i = 0; i < 5; ++i) {
-    distance = distance + analogRead(IRPin);
-    delay(100);
+void loop()
+{
+  int sensorStatus = digitalRead(IRSensor);
+  static unsigned long timer = millis();
+  
+  if (i > 6) i = 0;
+  
+  if (millis() - timer > 3000) {
+    i++;
+    timer = millis();
+    myDFPlayer.play(i);  //Play next mp3 every 3 second.
   }
-  distance = (int) distance / 5;
-  Serial.println( distance );
-  distance < 1300 ? a = 0 : a = 1;
-  //distance = analogRead(IRPin);
-
-  Serial.println( a );
-  //delay(500);
 }
