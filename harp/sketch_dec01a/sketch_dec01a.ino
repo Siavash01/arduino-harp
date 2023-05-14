a5f820b3c6d8402a16d354a3e01385fb0f1dce87
Siavash <kosarisiavash4@gmail.com>
2023-05-14 22:17:01 +0330

    created a state machine and added interrupt

--- harp/sketch_dec01a/sketch_dec01a.ino
+++ harp/sketch_dec01a/sketch_dec01a.ino
@@ -2,56 +2,22 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-int cnt = 0;
-#define IRSensorC4 0
-#define IRSensorD4 1
-#define IRSensorE4 2
-#define IRSensorF4 3
-#define IRSensorG4 4
-#define IRSensorA4 5
-#define IRSensorB4 6
+#define READ 0b0
+#define PLAY 0b1
 
-#define C4 0
-#define D4 1
-#define E4 2
-#define F4 3
-#define G4 4
-#define A4 5
-#define B4 6
-
+int state;
 int sensorStatus[7];
+
+const byte ledPin = 13;
+const byte interruptPin = 2;
 
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 void printDetail(uint8_t type, int value);
 
-void setup()
-{
-  for (int i = 0; i < 7; ++i) {
-    pinMode(i, INPUT); // set pinmod for ir sensor to output
-  }
-  mySoftwareSerial.begin(9600);
-  
-  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
-    while(true);
-  }
-
-  
-  myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
-  
-  //----Set volume----
-  myDFPlayer.volume(25);  //Set volume value (0~30).
-  
-  //----Set different EQ----
-  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
-  
-  //----Set device we use SD as default----
-  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
-}
-
 void checkStatus() {
   for (int i = 0; i < 7; ++i) {
-    sensorStatus[i] = digitalRead(i);
+    sensorStatus[i] = digitalRead(i+2);
   }
 }
 
@@ -75,9 +41,57 @@ void playNote() {
   }
 }
 
+void interruptFunc() {
+  state = PLAY;
+}
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(interruptPin), interruptFunc, CHANGE);
+
+  Serial.begin(115200); // Init Serial at 115200 Baud Rate.
+  Serial.println("Serial Working"); // Test to check if serial is working or not
+ 
+  for (int i = 0; i < 7; ++i) {
+    pinMode(i, INPUT); // set pinmod for ir sensor to output
+  }
+
+  mySoftwareSerial.begin(9600);
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println("MID SETUP");
+    while(true);
+  }
+  Serial.println("MID SETUP END");
+
+  
+  myDFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
+  
+  //----Set volume----
+  myDFPlayer.volume(25);  //Set volume value (0~30).
+  
+  //----Set different EQ----
+  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  
+  //----Set device we use SD as default----
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  Serial.println("end setup");
+}
+
 void loop()
 {
-  checkStatus();
-  playNote();
-  clearStatus();
+  switch(state) {
+    case READ:
+      state = READ;
+      break;
+    case PLAY:
+      checkStatus();
+      playNote();
+      clearStatus();
+      state = READ;
+      break;
+  }
 }
